7068ba1b5759dfe873808205865de38c3825169c
SeungJun_Song <john091122@naver.com>
2026-04-03 21:49:52 +0900

    refactor: 패키지 구조 재편성 (단일 계층 → 멀티 모듈 구조)

--- src/main/java/com/festapp/dashboar/DashBoarApplication.java
+++ src/main/java/com/festapp/dashboar/DashBoarApplication.java
@@ -1,3 +1,4 @@
+// Dashboard 애플리케이션 메인 클래스 - Spring Boot 애플리케이션의 진입점
 package com.festapp.dashboar;
 
 import org.springframework.boot.SpringApplication;
--- src/test/java/com/festapp/dashboar/DashBoarApplicationTests.java
+++ src/test/java/com/festapp/dashboar/DashBoarApplicationTests.java
@@ -1,3 +1,4 @@
+// 애플리케이션 통합 테스트 - Spring Boot 애플리케이션 컨텍스트 로드 테스트
 package com.festapp.dashboar;
 
 import org.junit.jupiter.api.Test;
